44ef8c69d39f31309ffef7eb6308b499e98fdb9c
cdeevfrr <cdeevfrr@users.noreply.github.com>
2017-11-10 18:43:51 -0500

    Add files via upload
    tuplegenerator sorts functions so that they can be easily used when generating tuples.

--- TupleGenerator.java
+++ TupleGenerator.java
@@ -10,7 +10,7 @@ public class TupleGenerator {
 
     int numAttributes;
     int[] numAttributeValues;
-    ProbabilisticFunction[][] functions; // pairs, for example [[1,2],[3]], where the r.h.s
+    ProbabilisticFunction[] functions; // pairs, for example [[1,2],[3]], where the r.h.s
     // functionally depends on the l.h.s. In other words, for this example
     // the attributes 1 and 2 functionally determine attribute 3.
 
@@ -24,9 +24,15 @@ public class TupleGenerator {
         To ensure good probabilities for everything, we need to generate each l.h.s
         before we generate r.h.s for any function. This can be accomplished
         by doing a topological sort on the functions where
-        f1 < f2 whenever f1 determines the l.h.s of f2. In other words,
+        f1 < f2 whenever f1 determines the l.h.s of f2. 
+        For example, if 
+        f1 = A -> B
+        f2 = B -> C
+        then values for A (f1) have to be generated first, and we can't
+        generate values according to f2 until that has happened.
+        In other words,
 
-            f1.rhs intersect f2.lhs neq emptyset.
+            f1.rhs intersect f2.lhs neq emptyset ==> f1 < f2
 
         */
 
@@ -35,9 +41,10 @@ public class TupleGenerator {
             public int compare(ProbabilisticFunction f1, ProbabilisticFunction f2){
                 for(Integer i : f2.lhs){
                     if(f1.rhs.contains(i)){
-
+                    	return -1;
                     }
                 }
+                return 0;
             }
         });
 
@@ -59,13 +66,10 @@ public class TupleGenerator {
      */
     public int[] makeTuple(){
         currentTuple = new int[numAttributes];
-        if(functions.length > 1){
-            throw new RuntimeException("Multiple functions are not supported yet");
-        }
         for(int i = 0; i < functions.length ; i ++){
-            if(r.nextDouble() > probabilities[i]){
+            if(r.nextDouble() > functions[i].probability){
                 //We want to enforce function i.
-
+            	
             }
         }
     }
